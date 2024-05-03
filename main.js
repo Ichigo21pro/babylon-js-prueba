43e2e6f1a86e4e24f7355065d9a016e66770e458
Ichigo21pro <aiglegon@gmail.com>
2024-05-03 11:17:19 +0100

    probando cosas de elementos, a continuacion texto

--- main.js
+++ main.js
@@ -8,11 +8,11 @@
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-/////////////////////// imports //////////////////////
+///////////////////////////////////////////// imports ///////////////////////////////////////////
 //
 import * as BABYLON from "@babylonjs/core";
 //
-///////////////// variables globales ////////////////
+/////////////////////////////////////// variables globales /////////////////////////////////////
 //
 // recogemos el canvas de index.html
 const canvas = document.getElementById("renderCanvas");
@@ -20,6 +20,7 @@ const canvas = document.getElementById("renderCanvas");
 // creamos la engine ("cabeza") del proyecto
 const engine = new BABYLON.Engine(canvas);
 //
+///////////////////////////////////////////// SCENE ////////////////////////////////////////////
 //creamos la escena
 const createScene = function () {
   //metemos todo lo necesario, entre ello la escena en si con la logica de babylon
@@ -27,19 +28,58 @@ const createScene = function () {
   //
   //añadimos la camara:
   scene.createDefaultCameraOrLight(true, false, true);
-  const box = new BABYLON.MeshBuilder.CreateBox();
   //
+  //añadimos elementos (ejemplos en orden de Complicado - Basicos)
+  //
+  /*const groundFromHM = new BABYLON.MeshBuilder.CreateGroundFromHeightMap(
+    "PrimerMapaTextura",
+    "/heightmap.png",
+    { height: 10, width: 10, subdivisions: 50, maxHeight: 2 }
+  );
+  groundFromHM.material = new BABYLON.StandardMaterial();
+  groundFromHM.material.wireframe = true;*/
+  /*const ground = new BABYLON.MeshBuilder.CreateGround("PrimerSuelo", {
+    height: 10,
+    width: 10,
+    subdivisions: 5, //poligonos visuales (no visible a no ser que se aplique lo de abajo)
+    subdivisionsX: 10,
+  });
+  //para que se vea los poligonos de subdivisions
+  ground.material = new BABYLON.StandardMaterial();
+  ground.material.wireframe = true;*/
+  /*const sphere = new BABYLON.MeshBuilder.CreateSphere(
+    "PrimeraEsfera",
+    {
+      segments: 20, //segementos, los "poligonos visuales" (parece que se vea "pixelado") cuantos más cantidad más detallado el objeto
+      diameter: 0.3, //diametro de la esfera
+      diameterY: 0.4, //diametro en eje Y
+    },
+    scene
+  ); //esfera*/
+  /*const box = new BABYLON.MeshBuilder.CreateBox("PrimerCubo", {
+    size: 0.2,
+    width: 0.2,
+    height: 0.3,
+    depth: 0.4,
+    faceColors: [new BABYLON.Color4(1, 0, 0, 1), BABYLON.Color3.Green()], //color de cada cara
+  }); //cubo*/
+  //
+  //añadimos texto (tiene que ser formato.json)
+  //
+
+  //
+  //return
   return scene;
   //
 };
 //
 const scene = createScene();
-//
+///////////////////////////////////////////// LOOP /////////////////////////////////////////////
 //actualizamos la escena
 engine.runRenderLoop(function () {
   scene.render();
 });
-//
+///////////////////////////////////////////// RESIZE ///////////////////////////////////////////
 //actualizamos el tamaño
 window.addEventListener("resize", function () {
   engine.resize();
